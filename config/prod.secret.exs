8d57680fc4071dc6ec16c926856970bfa6d8db59
ruchit <urunkar.r@husky.neu.edu>
2019-10-30 20:40:13 -0400

    first commit

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -32,7 +32,7 @@ config :timesheet1, Timesheet1Web.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :timesheet1, Timesheet1Web.Endpoint, server: true
+config :timesheet1, Timesheet1Web.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
